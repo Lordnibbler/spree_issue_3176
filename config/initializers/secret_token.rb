5e96a677eeb4dc0ae43a3703c810cbd3a38198a0
Ben Radler <ben@benradler.com>
2013-06-13 19:05:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeStore::Application.config.secret_token = '7158ace03705ffbd7f2823a9e187feaca8a482307ec80cee7b71f5b3c548a77ffe84b392af3b16468836e6ac34f19d37b001df1e20854589ff359ab3316bea84'
